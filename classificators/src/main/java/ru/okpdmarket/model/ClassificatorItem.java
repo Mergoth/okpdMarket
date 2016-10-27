48209f7b5b29eb069a2a90b397e785f9037f0201
thetamine <ethamine@gmail.com>
2016-10-28 00:34:13 +0300

    Cassandra config impl

--- classificators/src/main/java/ru/okpdmarket/model/ClassificatorItem.java
+++ classificators/src/main/java/ru/okpdmarket/model/ClassificatorItem.java
@@ -3,7 +3,10 @@ package ru.okpdmarket.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,13 +18,19 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ClassificatorItem implements Serializable {
 
-    private final String code;
-    private final String name;
+    private final String code =  "";
+    private final String name = "";
     @JsonIgnore
+    @Getter
+    @Setter
     private List<ClassificatorItem> children = new ArrayList<>();
     @JsonIgnore
+    @Getter
+    @Setter
     private Classificator classificator;
     @JsonIgnore
+    @Getter
+    @Setter
     private ClassificatorItem parent;
 
 }
