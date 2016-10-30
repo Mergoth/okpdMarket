263707728dc88f55f4ecaaf8eba8ec71958fdc58
Vladislav <nowspamhere@gmail.com>
2016-10-30 17:02:03 +0300

    Remove  unneccesary lines in gradle. Fix tests

--- classificators/src/test/java/ru/okpdmarket/controller/ClassificatorControllerTest.java
+++ classificators/src/test/java/ru/okpdmarket/controller/ClassificatorControllerTest.java
@@ -70,7 +70,7 @@ public class ClassificatorControllerTest {
 
     @Test
     public void getItems() throws Exception {
-        this.mockMvc.perform(get("/classificators/test").accept(MediaType.APPLICATION_JSON))
+        this.mockMvc.perform(get("/classificators/code").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(document("classificator-top-item"));
     }
@@ -82,7 +82,7 @@ public class ClassificatorControllerTest {
 
     @Test
     public void search() throws Exception {
-        this.mockMvc.perform(get("/classificators/test/search?query=Test query").accept(MediaType.APPLICATION_JSON))
+        this.mockMvc.perform(get("/classificators/code/search?query=Test query").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(document("classificator-search-results"));
     }
