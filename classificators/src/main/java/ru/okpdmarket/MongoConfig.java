14a83dd862897512d9e2274b20fcf03085203795
MOSCOW\Igor_Usachev <Igor_Usachev@epam.com>
2016-11-09 19:30:39 +0300

    Mongo migration -> config

---
+++ classificators/src/main/java/ru/okpdmarket/MongoConfig.java
@@ -0,0 +1,28 @@
+package ru.okpdmarket;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+
+/**
+ * Created by Igor_Usachev on 11/9/2016.
+ */
+@Configuration
+public class MongoConfig extends AbstractMongoConfiguration {
+
+    @Override
+    protected String getDatabaseName() {
+        return "okdp";
+    }
+
+    @Override
+    public Mongo mongo() throws Exception {
+        return new MongoClient("127.0.0.1", 27017);
+    }
+
+    @Override
+    protected String getMappingBasePackage() {
+        return "ru.okdp";
+    }
+}
