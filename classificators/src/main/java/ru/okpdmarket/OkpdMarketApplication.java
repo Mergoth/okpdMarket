fd75ce05f511b5bfc153225d2f3ae3f2b0f18336
Vlad <zzzxxxzzz@mail.ru>
2016-12-06 01:44:00 +0300

    Should work

--- classificators/src/main/java/ru/okpdmarket/OkpdMarketApplication.java
+++ classificators/src/main/java/ru/okpdmarket/OkpdMarketApplication.java
@@ -3,10 +3,12 @@ package ru.okpdmarket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@EnableDiscoveryClient
 @Configuration
 public class OkpdMarketApplication {
 
