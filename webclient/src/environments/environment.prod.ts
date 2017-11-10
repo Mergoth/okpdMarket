e3253dec123e64510e58aa01414880a58317ea36
Vlad <zzzxxxzzz@mail.ru>
2017-11-10 18:49:21 +0300

    1517: Setup infrastructure - update MongoDB and Auth mechanizm Update deployment for Docker Swarm and S3 hosting for webclient

--- webclient/src/environments/environment.prod.ts
+++ webclient/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-    serverUrl: "http://localhost"
+    serverUrl: "http://okpd-ExternalLoadB-1L4U70FMTJU87-1662606449.us-east-1.elb.amazonaws.com"
 };
