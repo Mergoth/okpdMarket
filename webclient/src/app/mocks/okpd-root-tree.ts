c32e15e0e2d5cb37e8af3a09c143af8e9c9884f0
Vlad <zzzxxxzzz@mail.ru>
2017-11-18 01:37:39 +0300

    1579: Switch to testcontainers - remove webclient. Rename some packages

--- webclient/src/app/mocks/okpd-root-tree.ts
+++
@@ -1,8 +0,0 @@
-import {Classificator} from "../domain/classificator";
-export const OKPD_ROOT_TREE: Classificator[] = [
-  {
-      code: "b",
-    name: "РАЗДЕЛ A ПРОДУКЦИЯ СЕЛЬСКОГО, ЛЕСНОГО И РЫБНОГО ХОЗЯЙСТВА "
-  }
-
-];
