bc5f5e0bc262e7f0f82fa189fdc343662aa918dc
Vlad <zzzxxxzzz@mail.ru>
2017-11-27 02:36:31 +0300

    Fix gateway CORS config Update Spring cloud version Healthchecks are more convinient in compose file, not Dockerfile

--- gateway/src/main/java/ru/okpdmarket/gateway/GatewayApplication.java
+++ gateway/src/main/java/ru/okpdmarket/gateway/GatewayApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
+@Configuration
 public class GatewayApplication {
 
     @Bean
