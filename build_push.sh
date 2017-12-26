3747ed4349eb699f344c2976f00df0fbe5db31e5
Vlad <zzzxxxzzz@mail.ru>
2017-12-26 23:52:15 +0300

    Infrastrucutre: mongodocker fix - fix build_push.sh

--- build_push.sh
+++ build_push.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-COMMIT=local
 REPO=115056349306.dkr.ecr.us-east-1.amazonaws.com
 TAG=$1
 
 bash <(aws ecr get-login --no-include-email --region us-east-1)
-
-#export CLASSIFICATORS_SERVICE=classificators
-#docker build -t $CLASSIFICATORS_SERVICE:$COMMIT ./classificators
-#docker tag $CLASSIFICATORS_SERVICE:$COMMIT $REPO/$CLASSIFICATORS_SERVICE:$TAG
-#docker push $REPO/$CLASSIFICATORS_SERVICE:$TAG
 
 export CLASSIFICATORS_MONGODB_SERVICE=classificators-mongodb
 docker build -t $REPO/$CLASSIFICATORS_MONGODB_SERVICE:$TAG ./classificators-mongodb
